384d4d50934e75219b84db1f3f5f61abb7b268b6
Andres Garcia <agarciactg@gmail.com>
2024-05-14 09:32:08 -0500

    FIX: Public Router - OK.

--- src/routes/PublicRoute.tsx
+++ src/routes/PublicRoute.tsx
@@ -2,8 +2,8 @@ import { Navigate, Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 const PublicRoute = () => {
-  const isLoggedIn = useSelector((state: any) => state.auth.authentication);
-  return isLoggedIn ? <Navigate to="/" /> : <Outlet />;
+  // const isLoggedIn = useSelector((state: any) => state.auth.authentication);
+  return <Outlet />
 };
 
 export default PublicRoute;
